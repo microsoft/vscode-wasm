118a506d46515d7cec69015209047a63b6d7198a
Dirk Baeumer <dirkb@microsoft.com>
2022-05-17 17:43:14 +0200

    Create terminal outside WASI

--- testbed/minimal/extension.ts
+++ testbed/minimal/extension.ts
@@ -6,13 +6,26 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { ExtensionContext } from 'vscode';
+import { EventEmitter, ExtensionContext, window } from 'vscode';
 
 import { WASI } from 'vscode-wasi/node';
 
-const wasi = WASI.create('WASI Minimal Example', { argv: ['Dirk', 'Bäumer'], env: { HOME: '/home/dbaeumer' } });
+const name = 'WASI Minimal Example';
+const ptyWriteEmitter: EventEmitter<string> = new EventEmitter();
+const terminal = window.createTerminal({ name, pty: {
+	onDidWrite: ptyWriteEmitter.event,
+	open: () => {
+		ptyWriteEmitter.fire(`\x1b[31m${name}\x1b[0m\r\n\r\n`);
+	},
+	close: () => {
+	},
+	handleInput: (_data: string) => {
+	}
+}});
+terminal.show();
 
 export async function activate(context: ExtensionContext) {
+	const wasi = WASI.create(name, { argv: ['Dirk', 'Bäumer'], env: { HOME: '/home/dbaeumer' }, ptyWriteEmitter });
 	const wasmFile = path.join(context.extensionPath, './rust/target/wasm32-wasi/debug/minimal.wasm');
 	const binary = fs.readFileSync(wasmFile);
 	const { instance } = await WebAssembly.instantiate(binary, {
