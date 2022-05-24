b1b742cb04219b0990369801adc4f6f47ca326cc
Dirk Baeumer <dirkb@microsoft.com>
2022-05-24 09:31:15 +0200

    Move cpp into minimal

--- testbed/minimal/rust/src/main.rs
+++ testbed/minimal/rust/src/main.rs
@@ -3,12 +3,20 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 use std::env;
-use std::io;
+use std::io::Write;
+use std::io::stdin;
+use std::io::stdout;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Hello, world {:?} !", args);
+
+    print!("Enter your name: ");
+    stdout().flush().unwrap();
+
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer);
-    println!("{:?}", buffer);
+    stdin().read_line(&mut buffer).unwrap();
+
+    print!("\nHello {}", buffer);
+    stdout().flush().unwrap();
 }
