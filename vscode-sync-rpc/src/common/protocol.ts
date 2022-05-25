e338d2e82e90d157d7d4baabe76b673545d2af22
Dirk Baeumer <dirkb@microsoft.com>
2022-05-25 21:30:36 +0200

    Added some comments

--- vscode-sync-rpc/src/common/protocol.ts
+++ vscode-sync-rpc/src/common/protocol.ts
@@ -3,13 +3,21 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-export type Requests = {
+export type Requests =
+{
+	/**
+	 * Write a string encoded using UTF8 to the terminal
+	 */
 	method: 'terminal/write';
 	params: {
 		binary: Uint8Array;
 	};
 	result: null;
 } | {
+	/**
+	 * Read from the terminal and provide the data as
+	 * a string encoded using UTF8
+	 */
 	method: 'terminal/read';
 	params: null;
 	result: Uint8Array;
