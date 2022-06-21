b387fef763a100de935f2df65cba0c06b6d6e764
Dirk Baeumer <dirkb@microsoft.com>
2022-06-21 12:36:03 +0200

    Trim down rust example

--- testbeds/rust/src/main.rs
+++ testbeds/rust/src/main.rs
@@ -2,34 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-// use std::env;
-// use std::io::Write;
-// use std::io::stdin;
-// use std::io::stdout;
 use std::fs;
 
-
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // println!("Hello, world {:?} !", args);
+    let contents = fs::read_to_string("/workspace/test.bat").expect("Something went wrong reading the file");
 
-    // print!("Enter your name: ");
-    // stdout().flush().unwrap();
-
-    // let mut buffer = String::new();
-    // stdin().read_line(&mut buffer).unwrap();
-
-    // print!("\nHello {}", buffer);
-    // stdout().flush().unwrap();
-
-// let _input_file =
-//         fs::File::open("/workspace/test.bat").map_err(|err| format!("error opening input {}: {}", "abc.txt", err)).unwrap();
-
-    // let contents =
-    //     fs::read_to_string("/workspace/test.bat").expect("Something went wrong reading the file");
-    // println!("Content:\n{}", contents);
-
-    for file in fs::read_dir("./workspace").unwrap() {
-        println!("{}", file.unwrap().path().display());
-    }
+    println!("Content:\n{}", contents);
 }
