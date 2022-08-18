5cc590a03b64e7965baae1e5c255fa26f38549bd
Dirk Baeumer <dirkb@microsoft.com>
2022-08-18 14:32:25 +0200

    More tracing for tests

--- vscode-sync-rpc/src/node/test/workers/uint8array.ts
+++ vscode-sync-rpc/src/node/test/workers/uint8array.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+/* eslint-disable no-console */
 import { TextEncoder } from 'util';
 import { parentPort  } from 'worker_threads';
 import { ServiceConnection } from '../../main';
@@ -13,7 +14,9 @@ if (parentPort === null) {
 
 const connection = new ServiceConnection<Requests>(parentPort);
 connection.onRequest('uint8array', (params, resultBuffer) => {
+	console.log(`Received request uint8array`);
 	resultBuffer.set(new TextEncoder().encode(params.p1));
+	console.log(`Returning result`);
 	return { errno: 0 };
 });
 connection.signalReady();
