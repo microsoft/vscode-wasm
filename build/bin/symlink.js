fe3df73e636b5fed3f13a74382579aacd94800b4
Dirk Baeumer <dirkb@microsoft.com>
2022-08-19 11:53:41 +0200

    More renames

--- build/bin/symlink.js
+++ build/bin/symlink.js
@@ -15,13 +15,13 @@ const node_modules = 'node_modules';
 (async function main() {
 	console.log('Symlinking node modules for development setup');
 
-	ln.softLink(path.join(root, 'vscode-sync-api-common'), path.join(root, 'vscode-sync-api-client', node_modules, '@vscode', 'sync-api-common'));
-	ln.softLink(path.join(root, 'vscode-sync-api-common'), path.join(root, 'vscode-sync-api-service', node_modules, '@vscode', 'sync-api-common'));
-	ln.softLink(path.join(root, 'vscode-sync-api-client'), path.join(root, 'wasi', node_modules, '@vscode', 'sync-api-client'));
+	ln.softLink(path.join(root, 'sync-api-common'), path.join(root, 'sync-api-client', node_modules, '@vscode', 'sync-api-common'));
+	ln.softLink(path.join(root, 'sync-api-common'), path.join(root, 'sync-api-service', node_modules, '@vscode', 'sync-api-common'));
+	ln.softLink(path.join(root, 'sync-api-client'), path.join(root, 'wasi', node_modules, '@vscode', 'sync-api-client'));
 
 	ln.softLink(path.join(root, 'wasi'), path.join(root, 'testbeds', node_modules, '@vscode', 'wasi'));
-	ln.softLink(path.join(root, 'vscode-sync-api-common'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-common'));
-	ln.softLink(path.join(root, 'vscode-sync-api-client'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-client'));
-	ln.softLink(path.join(root, 'vscode-sync-api-service'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-service'));
+	ln.softLink(path.join(root, 'sync-api-common'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-common'));
+	ln.softLink(path.join(root, 'sync-api-client'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-client'));
+	ln.softLink(path.join(root, 'sync-api-service'), path.join(root, 'testbeds', node_modules, '@vscode', 'sync-api-service'));
 
 })();
