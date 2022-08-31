af94a04b506bfa3b04c229d3212c1379e6c7a7e5
Dirk Baeumer <dirkb@microsoft.com>
2022-08-31 16:55:14 +0200

    More esbuild usage

--- sync-api-common/bin/esbuild.js
+++ sync-api-common/bin/esbuild.js
@@ -5,3 +5,21 @@
 //@ts-check
 
 const esbuild = require('esbuild');
+
+const workerTests = esbuild.build({
+	entryPoints: ['lib/browser/test/workers/main.js'],
+	outfile: 'dist/workerMain.js',
+	bundle: true,
+	define: { process: '{"env":{}}' },
+	target: 'es2020'
+}).catch(console.error);
+
+const testFixture = esbuild.build({
+	entryPoints: ['lib/browser/test/connection.test.js'],
+	outfile: 'dist/connection.test.js',
+	bundle: true,
+	define: { process: '{"env":{}}' },
+	target: 'es2020'
+}).catch(console.error);
+
+Promise.all([workerTests, testFixture]);
