963494cf2a776a0e97415ba37dda06a6fad77945
Dirk Baeumer <dirkb@microsoft.com>
2022-09-12 11:02:09 +0200

    Test modifications

--- testbeds/static-linking-wasi/main.c
+++ testbeds/static-linking-wasi/main.c
@@ -7,5 +7,15 @@ int bar() {
 int main() {
 	int result = bar() + bar();
 	printf("The result is %i\n", result);
+
+	// char ch, file_name[25];
+	// FILE * fp;
+
+	// fp = fopen(file_name, "r");
+	// while((ch = fgetc(fp)) != EOF)
+    // 	printf("%c", ch);
+
+	// fclose(fp);
+
 	return 0;
 }
