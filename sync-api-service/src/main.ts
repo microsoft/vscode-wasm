8298c81aa935d3058ba9877349f5d2bd897e4e48
Dirk Baeumer <dirkb@microsoft.com>
2022-09-15 18:19:14 +0200

    Publish sync-api-service@0.6.1

--- sync-api-service/src/main.ts
+++ sync-api-service/src/main.ts
@@ -3,4 +3,5 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
+export * from '@vscode/sync-api-common';
 export * from './apiService';
