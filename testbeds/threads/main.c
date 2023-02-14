3fa5f0597c19bfeaff800ff66bfc6b3249e5eb80
Dirk Bäumer <dirkb@microsoft.com>
2023-02-14 16:06:17 +0100

    Add pthread example (#42)

---
+++ testbeds/threads/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //Header file for sleep(). man 3 sleep for details.
+#include <pthread.h>
+
+// A normal C function that is executed as a thread
+// when its name is specified in pthread_create()
+void *myThreadFun(void *vargp)
+{
+	sleep(1);
+	printf("Printing GeeksQuiz from Thread \n");
+	return NULL;
+}
+
+int main()
+{
+	pthread_t thread_id;
+	printf("Before Thread\n");
+	pthread_create(&thread_id, NULL, myThreadFun, NULL);
+	pthread_join(thread_id, NULL);
+	printf("After Thread\n");
+	exit(0);
+}
