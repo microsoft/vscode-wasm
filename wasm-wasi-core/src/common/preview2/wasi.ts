8f59a1061d0f8dfdc721de2e19df8925d4eb6d18
Dirk Baeumer <dirkb@microsoft.com>
2023-06-29 13:30:09 +0200

    More preview2 meta model

--- wasm-wasi-core/src/common/preview2/wasi.ts
+++ wasm-wasi-core/src/common/preview2/wasi.ts
@@ -4,6 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 import RAL from '../ral';
 
+const utf8Decoder = RAL().TextDecoder.create('utf-8');
+const utf8Encoder = RAL().TextEncoder.create('utf-8');
+
 export interface Memory {
 	readonly buffer: ArrayBuffer;
 	readonly raw: Uint8Array;
@@ -11,8 +14,6 @@ export interface Memory {
 	alloc: (align: size, size: size) => ptr;
 	realloc: (ptr: ptr, oldSize: size, align: size, newSize: size) => ptr;
 }
-
-export type MemoryView = Omit<Memory, 'alloc' | 'realloc'>;
 
 export type encodings = 'utf-8' | 'utf-16' | 'latin1+utf-16';
 export interface Options {
@@ -24,45 +25,46 @@ function align(ptr: ptr, alignment: alignment): ptr {
 	return Math.ceil(ptr / alignment) * alignment;
 }
 
-export interface MetaXX<W,J, F = s32> {
+export interface Meta<W, J, F extends s32 | s64 | float32 | float64> {
 	readonly size: number;
 	readonly alignment: alignment;
-	flatten: (value: J) => F;
-	load: (memory: Memory, ptr: ptr<W>) => J;
-	loadFlattened: (value: F) => J;
+	flatten: (memory: Memory, value: W, result: F[]) => void;
+	load: (memory: Memory, ptr: ptr<W>, options: Options) => J;
+	loadFlattened: (memory: Memory, value: F | F[], options: Options) => J;
 	alloc: (memory: Memory) => ptr<W>;
-	store: (memory: MemoryView, ptr: ptr<W>, value: J) => void;
+	store: (memory: Memory, ptr: ptr<W>, value: J, options: Options) => void;
 }
-export interface MetaNumber<T> extends MetaXX<T, T, s32> {
+
+export interface MetaNumber<T> extends Meta<T, T, s32> {
 	readonly lowValue: T;
 	readonly highValue: T;
 }
 
-export interface MetaBigint<T> extends MetaXX<T, T, s64> {
+export interface MetaBigint<T> extends Meta<T, T, s64> {
 	readonly lowValue: T;
 	readonly highValue: T;
 }
 
 export type bool = number;
-export const bool: MetaXX<bool, boolean> = {
+export const bool: Meta<bool, boolean, s32> = {
 	size: 1,
 	alignment: 1,
-	flatten(value: boolean): s32 {
-		return value ? 1 : 0;
+	flatten(_memory, value: bool, result): void {
+		result.push(value);
 	},
-	load(memory: Memory, ptr: ptr<bool>): boolean {
+	load(memory, ptr: ptr<bool>): boolean {
 		return memory.view.getUint8(ptr) !== 0;
 	},
-	loadFlattened(value: s32): boolean {
-		if (value < 0) {
+	loadFlattened(_memory, value): boolean {
+		if (Array.isArray(value) || value < 0) {
 			throw new Error(`Invalid bool value ${value}`);
 		}
 		return value !== 0;
 	},
-	alloc(memory: Memory): ptr<bool> {
+	alloc(memory): ptr<bool> {
 		return memory.alloc(bool.size, bool.alignment);
 	},
-	store(memory: MemoryView, ptr: ptr<bool>, value: boolean): void {
+	store(memory, ptr: ptr<bool>, value: boolean): void {
 		memory.view.setUint8(ptr, value ? 1 : 0);
 	}
 };
@@ -75,22 +77,22 @@ export const u8:MetaNumber<u8> = {
 	lowValue: 0,
 	highValue: 255,
 
-	flatten(value: u8): s32 {
-		return value;
+	flatten(_memory, value: u8, result): void {
+		result.push(value);
 	},
-	load(memory: Memory, ptr: ptr<u8>): u8 {
+	load(memory, ptr: ptr<u8>): u8 {
 		return memory.view.getUint8(ptr);
 	},
-	loadFlattened(value: s32): u8 {
-		if (value < u8.lowValue || value > u8.highValue) {
+	loadFlattened(_memory, value): u8 {
+		if (Array.isArray(value) || value < u8.lowValue || value > u8.highValue) {
 			throw new Error(`Invalid u8 value ${value}`);
 		}
 		return value;
 	},
-	alloc(memory: Memory): ptr<u8> {
+	alloc(memory): ptr<u8> {
 		return memory.alloc(u8.size, u8.alignment);
 	},
-	store(memory: MemoryView, ptr: ptr<u8>, value: u8): void {
+	store(memory, ptr: ptr<u8>, value: u8): void {
 		memory.view.setUint8(ptr, value);
 	}
 };
@@ -103,14 +105,14 @@ export const u16: MetaNumber<u16> = {
 	lowValue: 0,
 	highValue: 65535,
 
-	flatten(value: u16): s32 {
-		return value;
+	flatten(_memory, value: u16, result): void {
+		result.push(value);
 	},
 	load(memory: Memory, ptr: ptr): u16 {
 		return memory.view.getUint16(ptr, true);
 	},
-	loadFlattened(value: s32): u16 {
-		if (value < u16.lowValue || value > u16.highValue) {
+	loadFlattened(_memory, value): u16 {
+		if (Array.isArray(value) || value < u16.lowValue || value > u16.highValue) {
 			throw new Error(`Invalid u16 value ${value}`);
 		}
 		return value;
@@ -118,7 +120,7 @@ export const u16: MetaNumber<u16> = {
 	alloc(memory: Memory): ptr<u16> {
 		return memory.alloc(u16.size, u16.alignment);
 	},
-	store(memory: MemoryView, ptr: ptr<u16>, value: u16): void {
+	store(memory, ptr: ptr<u16>, value: u16): void {
 		memory.view.setUint16(ptr, value, true);
 	}
 };
@@ -131,17 +133,17 @@ export const u32: MetaNumber<u32> = {
 	lowValue: 0,
 	highValue: 4294967295, // 2 ^ 32 - 1
 
-	flatten(value: u32): s32 {
+	flatten(_memory, value: u32, result): void {
 		if (value > s32.highValue) {
 			throw new Error(`Can't flatten u32 ${value} to s32`);
 		}
-		return value;
+		result.push(value);
 	},
 	load(memory: Memory, ptr: ptr): u32 {
 		return memory.view.getUint32(ptr, true);
 	},
-	loadFlattened(value: s32): u32 {
-		if (value < u32.lowValue) {
+	loadFlattened(_memory, value): u32 {
+		if (Array.isArray(value) || value < u32.lowValue) {
 			throw new Error(`Invalid u32 value ${value}`);
 		}
 		return value;
@@ -149,7 +151,7 @@ export const u32: MetaNumber<u32> = {
 	alloc(memory: Memory): ptr<u32> {
 		return memory.alloc(u32.size, u32.alignment);
 	},
-	store(memory: MemoryView, ptr: ptr<u32>, value: u32): void {
+	store(memory: Memory, ptr: ptr<u32>, value: u32): void {
 		memory.view.setUint32(ptr, value, true);
 	}
 };
@@ -162,17 +164,17 @@ export const u64: MetaBigint<u64> = {
 	lowValue: 0n,
 	highValue: 18446744073709551615n, // 2 ^ 64 - 1
 
-	flatten(value: u64): s64 {
+	flatten(_memory, value: u64, result): void {
 		if (value > s64.highValue) {
 			throw new Error(`Can't flatten u64 ${value} to s64`);
 		}
-		return value;
+		result.push(value);
 	},
 	load(memory: Memory, ptr: ptr): u64 {
 		return memory.view.getBigUint64(ptr, true);
 	},
-	loadFlattened(value: s64): u64 {
-		if (value < u64.lowValue) {
+	loadFlattened(_memory, value): u64 {
+		if (Array.isArray(value) || value < u64.lowValue) {
 			throw new Error(`Invalid u64 value ${value}`);
 		}
 		return value;
@@ -180,179 +182,170 @@ export const u64: MetaBigint<u64> = {
 	alloc(memory: Memory): ptr<u64> {
 		return memory.alloc(u64.size, u64.alignment);
 	},
-	store(memory: MemoryView, ptr: ptr<u64>, value: u64): void {
+	store(memory: Memory, ptr: ptr<u64>, value: u64): void {
 		memory.view.setBigUint64(ptr, value, true);
 	}
 };
 
 export type s8 = number;
-export namespace s8 {
-	export const size = 1;
-	export const alignment: alignment = 1;
+export const s8: MetaNumber<u8> = {
+	size: 1,
+	alignment: 1,
 
-	export const lowValue = -128; // -2 ^ 7
-	export const highValue = 127; // 2 ^ 7 - 1
+	lowValue: -128, // -2 ^ 7
+	highValue: 127, // 2 ^ 7 - 1
 
-	export function flatten(value: s8): s32 {
+	flatten(_memory, value: s8): s32 {
 		return value;
-	}
-	export function load(memory: Memory, ptr: ptr): s8 {
+	},
+	load(memory: Memory, ptr: ptr): s8 {
 		return memory.view.getInt8(ptr);
-	}
-	export function loadFlattened(value: s32): s8 {
-		if (value < lowValue || value > highValue) {
+	},
+	loadFlattened(_memory, value): s8 {
+		if (Array.isArray(value) || value < s8.lowValue || value > s8.highValue) {
 			throw new Error(`Invalid s8 value ${value}`);
 		}
 		return value;
-	}
-	export function alloc(memory: Memory): ptr<s8> {
-		return memory.alloc(size, alignment);
-	}
-	export function store(memory: MemoryView, ptr: ptr<s8>, value: s8): void {
+	},
+	alloc(memory: Memory): ptr<s8> {
+		return memory.alloc(s8.size, s8.alignment);
+	},
+	store(memory: Memory, ptr: ptr<s8>, value: s8): void {
 		memory.view.setInt8(ptr, value);
 	}
-}
+};
 
 export type s16 = number;
-export namespace s16 {
-	export const size = 2;
-	export const alignment: alignment = 2;
+export const s16: MetaNumber<s16> = {
+	size: 2,
+	alignment: 2,
 
-	export const lowValue = -32768; // -2 ^ 15
-	export const highValue = 32767; // 2 ^ 15 - 1
+	lowValue: -32768, // -2 ^ 15
+	highValue: 32767, // 2 ^ 15 - 1
 
-	export function flatten(value: s16): s32 {
+	flatten(_memory, value: s16): s32 {
 		return value;
-	}
-	export function load(memory: Memory, ptr: ptr): s16 {
+	},
+	load(memory: Memory, ptr: ptr): s16 {
 		return memory.view.getInt16(ptr, true);
-	}
-	export function loadFlattened(value: s32): s16 {
-		if (value < lowValue || value > highValue) {
+	},
+	loadFlattened(_memory, value): s16 {
+		if (Array.isArray(value) || value < s16.lowValue || value > s16.highValue) {
 			throw new Error(`Invalid s16 value ${value}`);
 		}
 		return value;
-	}
-	export function alloc(memory: Memory): ptr<s16> {
-		return memory.alloc(size, alignment);
-	}
-	export function store(memory: MemoryView, ptr: ptr<s16>, value: s16): void {
+	},
+	alloc(memory: Memory): ptr<s16> {
+		return memory.alloc(s16.size, s16.alignment);
+	},
+	store(memory: Memory, ptr: ptr<s16>, value: s16): void {
 		memory.view.setInt16(ptr, value, true);
 	}
-}
+};
 
 export type s32 = number;
-export namespace s32 {
-	export const size = 4;
-	export const alignment: alignment = 4;
+export const s32: MetaNumber<s32> = {
+	size: 4,
+	alignment: 4,
 
-	export const lowValue = -2147483648; // -2 ^ 31
-	export const highValue = 2147483647; // 2 ^ 31 - 1
+	lowValue: -2147483648, // -2 ^ 31
+	highValue: 2147483647, // 2 ^ 31 - 1
 
-	export function flatten(value: s32): s32 {
+	flatten(_memory, value: s32): s32 {
 		return value;
-	}
-	export function load(memory: Memory, ptr: ptr): s32 {
+	},
+	load(memory: Memory, ptr: ptr): s32 {
 		return memory.view.getInt32(ptr, true);
-	}
-	export function loadFlattened(value: s32): s32 {
-		if (value < lowValue || value > highValue) {
-			throw new Error(`Invalid s32 value ${value}`);
-		}
-		return value;
-	}
-	export function alloc(memory: Memory): ptr<s32> {
-		return memory.alloc(size, alignment);
-	}
-	export function store(memory: MemoryView, ptr: ptr<s32>, value: s32): void {
+	},
+	loadFlattened(_memory, value): s32 {
+		return value as s32;
+	},
+	alloc(memory: Memory): ptr<s32> {
+		return memory.alloc(s32.size, s32.alignment);
+	},
+	store(memory, ptr: ptr<s32>, value: s32): void {
 		memory.view.setInt32(ptr, value, true);
 	}
-}
+};
 
 export type s64 = bigint;
-export namespace s64 {
-	export const size = 8;
-	export const alignment: alignment = 8;
+export const s64: MetaBigint<s64> = {
+	size: 8,
+	alignment: 8,
 
-	export const lowValue = -9223372036854775808n; // -2 ^ 63
-	export const highValue = 9223372036854775807n; // 2 ^ 63 - 1
+	lowValue: -9223372036854775808n, // -2 ^ 63
+	highValue: 9223372036854775807n, // 2 ^ 63 - 1
 
-	export function flatten(value: s64): s64 {
-		if (value < lowValue || value > highValue) {
-			throw new Error(`Can't flatten s64 ${value} to s64`);
-		}
+	flatten(_memory, value: s64): s64 {
 		return value;
-	}
-	export function load(memory: Memory, ptr: ptr<s64>): s64 {
+	},
+	load(memory: Memory, ptr: ptr<s64>): s64 {
 		return memory.view.getBigInt64(ptr, true);
-	}
-	export function loadFlattened(value: s64): s64 {
-		if (value < lowValue || value > highValue) {
-			throw new Error(`Invalid s64 value ${value}`);
-		}
-		return value;
-	}
-	export function alloc(memory: Memory): ptr<s64> {
-		return memory.alloc(size, alignment);
-	}
-	export function store(memory: MemoryView, ptr: ptr<s64>, value: s64): void {
+	},
+	loadFlattened(_memory, value): s64 {
+		return value as s64;
+	},
+	alloc(memory: Memory): ptr<s64> {
+		return memory.alloc(s64.size, s64.alignment);
+	},
+	store(memory: Memory, ptr: ptr<s64>, value: s64): void {
 		memory.view.setBigInt64(ptr, value, true);
-	}
-}
+	},
+};
 
 export type float32 = number;
-export namespace float32 {
-	export const size = 4;
-	export const alignment: alignment = 4;
+export const float32 = {
+	size: 4,
+	alignment: 4,
 
-	export const lowValue = -3.4028234663852886e+38;
-	export const highValue = 3.4028234663852886e+38;
-}
+	lowValue: -3.4028234663852886e+38,
+	highValue: 3.4028234663852886e+38,
+};
 
 export type float64 = number;
-export namespace float64 {
-	export const size = 8;
-	export const alignment: alignment = 8;
+export const float64 = {
+	size: 8,
+	alignment: 8,
 
-	export const lowValue = -1 * Number.MAX_VALUE;
-	export const highValue = Number.MAX_VALUE;
-}
+	lowValue: -1 * Number.MAX_VALUE,
+	highValue: Number.MAX_VALUE
+};
 
 export type byte = u8;
-export namespace byte {
-	export const size = u8.size;
-	export const alignment: alignment = u8.alignment;
+export const byte: Meta<byte, byte, s32> = {
+	size: u8.size,
+	alignment: u8.alignment,
 
-	export const flatten = u8.flatten;
-	export const load = u8.load;
-	export const loadFlattened = u8.loadFlattened;
-	export const alloc = u8.alloc;
-	export const store = u8.store;
-}
+	flatten: u8.flatten,
+	load: u8.load,
+	loadFlattened: u8.loadFlattened,
+	alloc: u8.alloc,
+	store: u8.store
+};
 
 export type size = u32;
-export namespace size {
-	export const size = u32.size;
-	export const alignment: alignment = u32.alignment;
+export const size: Meta<size, size, s32> = {
+	size: u32.size,
+	alignment: u32.alignment,
 
-	export const flatten = u32.flatten;
-	export const load = u32.load;
-	export const loadFlattened = u32.loadFlattened;
-	export const alloc = u32.alloc;
-	export const store = u32.store;
-}
+	flatten: u32.flatten,
+	load: u32.load,
+	loadFlattened: u32.loadFlattened,
+	alloc: u32.alloc,
+	store: u32.store
+};
 
 export type ptr<_type = u8> = u32;
-export namespace ptr {
-	export const size = u32.size;
-	export const alignment: alignment = u32.alignment;
+export const ptr: Meta<ptr, ptr, s32> = {
+	size: u32.size,
+	alignment: u32.alignment,
 
-	export const flatten = u32.flatten;
-	export const load = u32.load;
-	export const loadFlattened = u32.loadFlattened;
-	export const alloc = u32.alloc;
-	export const store = u32.store;
-}
+	flatten: u32.flatten,
+	load: u32.load,
+	loadFlattened: u32.loadFlattened,
+	alloc: u32.alloc,
+	store: u32.store
+};
 
 export interface char {
 
@@ -363,34 +356,38 @@ export interface wstring {
 	codeUnits: u32;
 }
 
-export namespace wstring {
-
-	export const size = 8;
-	export const alignment: alignment = 4;
-
-	const offsets = {
+namespace $wstring {
+	export const offsets = {
 		data: 0,
 		codeUnits: 4
 	};
+}
 
-	const utf8Decoder = RAL().TextDecoder.create('utf-8');
-	const utf8Encoder = RAL().TextEncoder.create('utf-8');
+export const wstring: Meta<ptr<wstring>, string, s32> = {
+	size: 8,
+	alignment:  4,
 
-	export function flatten(memory: Memory, ptr: ptr<wstring>): [ptr, u32] {
+	flatten(memory, ptr): [ptr, u32] {
 		const view = memory.view;
+		const offsets = $wstring.offsets;
 		const dataPtr: ptr =  view.getUint32(ptr + offsets.data);
 		const codeUnits: u32 = view.getUint32(ptr + offsets.codeUnits);
 		return [dataPtr, codeUnits];
-	}
+	},
 
-	export function load(memory: Memory, ptr: ptr<wstring>, encoding: encodings): string {
-		return loadFlattened(memory, ...flatten(memory, ptr), encoding);
-	}
+	load(memory: Memory, ptr: ptr<wstring>, options: Options): string {
+		return wstring.loadFlattened(memory, wstring.flatten(memory, ptr), options);
+	},
 
-	export function loadFlattened(memory: Memory, data: ptr, codeUnits: u32, encoding: encodings): string {
+	loadFlattened(memory, values, options: Options): string {
+		const encoding = options.encoding;
 		if (encoding === 'latin1+utf-16') {
 			throw new Error('latin1+utf-16 encoding not yet supported');
 		}
+		if (!Array.isArray(values) || values.length !== 2) {
+			throw new Error('Invalid flatten values');
+		}
+		const [data, codeUnits] = values;
 		if (encoding === 'utf-8') {
 			const byteLength = codeUnits;
 			return utf8Decoder.decode(new Uint8Array(memory.buffer, data, byteLength));
@@ -399,14 +396,15 @@ export namespace wstring {
 		} else {
 			throw new Error('Unsupported encoding');
 		}
-	}
+	},
 
-	export function alloc(memory: Memory): ptr<wstring> {
-		return memory.alloc(size, alignment);
-	}
+	alloc(memory: Memory): ptr<wstring> {
+		return memory.alloc(wstring.size, wstring.alignment);
+	},
 
-	export function store(memory: Memory, ptr: ptr<wstring>, str: string, options: Options): void {
+	store(memory: Memory, ptr: ptr<wstring>, str: string, options: Options): void {
 		const { encoding } = options;
+		const offsets = $wstring.offsets;
 		if (encoding === 'latin1+utf-16') {
 			throw new Error('latin1+utf-16 encoding not yet supported');
 		}
@@ -429,8 +427,8 @@ export namespace wstring {
 		} else {
 			throw new Error('Unsupported encoding');
 		}
-	}
-}
+	},
+};
 
 interface MetaInfo<T> {
 	readonly size: number;
@@ -485,7 +483,7 @@ interface TestRecord {
 
 namespace TestRecord {
 	const fields: RecordField[] = [];
-	const meta: [string, MetaInfo<any>][] = [['a', u8], ['b', u32], ['c', u8]];
+	const meta: [string, MetaInfo<any>][] = [['a', u8], ['b', u32], ['c', u8], ['d', wstring]];
 	let offset = 0;
 	for (const item of meta) {
 		const [name, meta] = item;
