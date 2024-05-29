36b29e17bf1e60125dbf02b27ff6bf55a681687d
Dirk Baeumer <dirkb@microsoft.com>
2024-05-29 15:54:31 +0200

    Use bind in sync case

--- testbeds/component-model/src/extension.ts
+++ testbeds/component-model/src/extension.ts
@@ -2,7 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { WasmContext, Memory } from '@vscode/wasm-component-model';
 import * as vscode from 'vscode';
 
 import { calculator, Types } from './calculator';
@@ -12,10 +11,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	const bits = await vscode.workspace.fs.readFile(filename);
 	const module = await WebAssembly.compile(bits);
 
-	const wasmContext: WasmContext.Default = new WasmContext.Default();
-	const instance = await WebAssembly.instantiate(module, {});
-	wasmContext.initialize(new Memory.Default(instance.exports));
-	const api = calculator._.exports.bind(instance.exports as calculator._.Exports, wasmContext);
+	const api = await calculator._.bind({}, module);
 
 	vscode.commands.registerCommand('testbed-component-model.run', () => {
 		console.log(`Add ${api.calc(Types.Operation.Add({ left: 1, right: 2}))}`);
