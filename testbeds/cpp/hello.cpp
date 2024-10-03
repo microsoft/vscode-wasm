4e0f215ffadff33fb1a2d77a06673ec59e967af1
Dirk Baeumer <dirkb@microsoft.com>
2024-10-03 17:06:14 +0200

    Add EOT to terminal

--- testbeds/cpp/hello.cpp
+++ testbeds/cpp/hello.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
+
+#include <stdio.h>
 
 int main()
 {
-	cout << "Hello World\n";
+	// cout << "Hello World\n";
+	// string input;
+	// cout << "Enter a string: ";
+	// cin >> input;
+	// cout << "You entered: " << input << endl;
+	int c;
+	while ((c = getchar()) != EOF) {
+		printf("%c", c);
+	}
 	return 0;
 }
